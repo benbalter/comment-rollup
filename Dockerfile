aca13b75bb8bcf3f1a14c35a2526dd1bff5506bc
Ben Balter <ben@balter.com>
2022-08-26 11:31:02 -0400

    move to ts action

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM ruby:3.1.2
-
-COPY Gemfile ./
-RUN bundle install
-
-COPY rollup.rb /rollup.rb
-
-ENTRYPOINT ["/rollup.rb"]
---
+++ src/main.ts
@@ -0,0 +1,72 @@
+import * as github from '@actions/github';
+import * as core from '@actions/core';
+import 'dotenv/config';
+
+const summary = 'Comment rollup';
+
+function hasLabel(labels: (string | { name?: string; })[], label: string): boolean {
+  let labelArray: { name?: string; }[];
+
+  if (typeof labels === 'string') {
+    labelArray = Array({ name: labels });
+  } else {
+    labelArray = <{ name?: string; }[]>labels;
+  }
+
+  return labelArray.some((candidate) => candidate.name === label);
+}
+
+function issueBody(issue: { body?: string; }, comments: { body?: string; }[]): string {
+  const rollupRegex = new RegExp(`<details>\\s*<summary>\\s*${summary}\\s*</summary>[\\s\\S]*?</details>`, 'im');
+  let body: string;
+  let rollup = comments.map((comment) => comment.body).join('\n\n');
+  rollup = `<details><summary>${summary}</summary>\n\n${rollup}\n\n</details>`;
+
+  if (issue.body?.match(rollupRegex)) {
+    body = issue.body.replace(rollupRegex, rollup);
+  } else {
+    body = `${issue.body}\n\n${rollup}`;
+  }
+
+  return body;
+}
+
+async function run(): Promise<void> {
+  const token = core.getInput('token', { required: true });
+  const label = core.getInput('label');
+  const issueNumber = parseInt(core.getInput('issue_number', { required: true }), 10);
+
+  const { context } = github;
+  const octokit = github.getOctokit(token);
+  const octokitArgs = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issueNumber,
+  };
+
+  const { data: issue } = await octokit.rest.issues.get(octokitArgs);
+
+  if (label && !hasLabel(issue.labels, label)) {
+    core.info(`Issue ${issue.title} does not have label ${label}. Skipping.`);
+    return;
+  }
+
+  const { data: comments } = await octokit.rest.issues.listComments(octokitArgs);
+
+  if (comments.length === 0) {
+    core.warning(`Issue ${issue.title} does not have any comments. Skipping.`);
+    return;
+  }
+
+  const body = issueBody(issue, comments);
+  octokit.rest.issues.update({ ...octokitArgs, body });
+  core.notice(`Rolled up ${comments.length} comments to issue ${issue.title}`);
+}
+
+try {
+  run();
+} catch (error) {
+  if (error instanceof Error) core.setFailed(error.message);
+}
+
+export { hasLabel, issueBody, run };
