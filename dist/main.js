d5dbb992368f770e1b183925e909eba7f08af471
Ben Balter <ben@balter.com>
2022-08-26 12:39:31 -0400

    fix dist

--- dist/main.js
+++ dist/main.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -33,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run = exports.issueBody = exports.hasLabel = void 0;
-const github = __importStar(require("@actions/github"));
+const github_1 = require("@actions/github");
 const core_1 = require("@actions/core");
 require("dotenv/config");
 const summary = "Comment rollup";
@@ -68,8 +45,8 @@ function run() {
         const token = (0, core_1.getInput)("token", { required: true });
         const label = (0, core_1.getInput)("label");
         const issueNumber = parseInt((0, core_1.getInput)("issue_number", { required: true }), 10);
-        const context = github.context;
-        const octokit = github.getOctokit(token);
+        const context = github_1.context;
+        const octokit = (0, github_1.getOctokit)(token);
         const octokitArgs = Object.assign(Object.assign({}, context.repo), { issue_number: issueNumber });
         const { data: issue } = yield octokit.rest.issues.get(octokitArgs);
         if (label && !hasLabel(issue.labels, label)) {
