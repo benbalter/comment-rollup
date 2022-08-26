596c07722417adc23d1f2e8bcf951a701af6e761
Ben Balter <ben@balter.com>
2022-08-26 12:03:32 -0400

    add mock test

---
+++ __tests__/main.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "@jest/globals";
+
+test("true", () => {
+  expect(true).toBe(true);
+});
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+}
