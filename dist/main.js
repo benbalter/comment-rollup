0b8a2f883e6b026e34a59f363cab2499e6a1c0fa
Ben Balter <ben@balter.com>
2022-08-26 11:44:25 -0400

    lint fixes

---
+++ dist/main.js
@@ -0,0 +1,80 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.run = exports.issueBody = exports.hasLabel = void 0;
+const github_1 = __importDefault(require("@actions/github"));
+const core_1 = require("@actions/core");
+require("dotenv/config");
+const summary = "Comment rollup";
+function hasLabel(labels, label) {
+    let labelArray;
+    if (typeof labels === "string") {
+        labelArray = Array({ name: labels });
+    }
+    else {
+        labelArray = labels;
+    }
+    return labelArray.some((candidate) => candidate.name === label);
+}
+exports.hasLabel = hasLabel;
+function issueBody(issue, comments) {
+    var _a;
+    const rollupRegex = new RegExp(`<details>\\s*<summary>\\s*${summary}\\s*</summary>[\\s\\S]*?</details>`, "im");
+    let body;
+    let rollup = comments.map((comment) => comment.body).join("\n\n");
+    rollup = `<details><summary>${summary}</summary>\n\n${rollup}\n\n</details>`;
+    if ((_a = issue.body) === null || _a === void 0 ? void 0 : _a.match(rollupRegex)) {
+        body = issue.body.replace(rollupRegex, rollup);
+    }
+    else {
+        body = `${issue.body}\n\n${rollup}`;
+    }
+    return body;
+}
+exports.issueBody = issueBody;
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const token = (0, core_1.getInput)("token", { required: true });
+        const label = (0, core_1.getInput)("label");
+        const issueNumber = parseInt((0, core_1.getInput)("issue_number", { required: true }), 10);
+        const { context } = github_1.default;
+        const octokit = github_1.default.getOctokit(token);
+        const octokitArgs = {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issueNumber,
+        };
+        const { data: issue } = yield octokit.rest.issues.get(octokitArgs);
+        if (label && !hasLabel(issue.labels, label)) {
+            (0, core_1.info)(`Issue ${issue.title} does not have label ${label}. Skipping.`);
+            return;
+        }
+        const { data: comments } = yield octokit.rest.issues.listComments(octokitArgs);
+        if (comments.length === 0) {
+            (0, core_1.warning)(`Issue ${issue.title} does not have any comments. Skipping.`);
+            return;
+        }
+        const body = issueBody(issue, comments);
+        octokit.rest.issues.update(Object.assign(Object.assign({}, octokitArgs), { body }));
+        (0, core_1.notice)(`Rolled up ${comments.length} comments to issue ${issue.title}`);
+    });
+}
+exports.run = run;
+try {
+    run();
+}
+catch (error) {
+    if (error instanceof Error)
+        (0, core_1.setFailed)(error.message);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,34 @@
-import * as github from '@actions/github';
-import * as core from '@actions/core';
-import 'dotenv/config';
+import github from "@actions/github";
+import { getInput, info, warning, notice, setFailed } from "@actions/core";
+import "dotenv/config";
 
-const summary = 'Comment rollup';
+const summary = "Comment rollup";
 
-function hasLabel(labels: (string | { name?: string; })[], label: string): boolean {
-  let labelArray: { name?: string; }[];
+function hasLabel(
+  labels: (string | { name?: string })[],
+  label: string
+): boolean {
+  let labelArray: { name?: string }[];
 
-  if (typeof labels === 'string') {
+  if (typeof labels === "string") {
     labelArray = Array({ name: labels });
   } else {
-    labelArray = <{ name?: string; }[]>labels;
+    labelArray = <{ name?: string }[]>labels;
   }
 
   return labelArray.some((candidate) => candidate.name === label);
 }
 
-function issueBody(issue: { body?: string; }, comments: { body?: string; }[]): string {
-  const rollupRegex = new RegExp(`<details>\\s*<summary>\\s*${summary}\\s*</summary>[\\s\\S]*?</details>`, 'im');
+function issueBody(
+  issue: { body?: string | null },
+  comments: { body?: string }[]
+): string {
+  const rollupRegex = new RegExp(
+    `<details>\\s*<summary>\\s*${summary}\\s*</summary>[\\s\\S]*?</details>`,
+    "im"
+  );
   let body: string;
-  let rollup = comments.map((comment) => comment.body).join('\n\n');
+  let rollup = comments.map((comment) => comment.body).join("\n\n");
   rollup = `<details><summary>${summary}</summary>\n\n${rollup}\n\n</details>`;
 
   if (issue.body?.match(rollupRegex)) {
@@ -32,9 +41,12 @@ function issueBody(issue: { body?: string; }, comments: { body?: string; }[]): s
 }
 
 async function run(): Promise<void> {
-  const token = core.getInput('token', { required: true });
-  const label = core.getInput('label');
-  const issueNumber = parseInt(core.getInput('issue_number', { required: true }), 10);
+  const token = getInput("token", { required: true });
+  const label = getInput("label");
+  const issueNumber = parseInt(
+    getInput("issue_number", { required: true }),
+    10
+  );
 
   const { context } = github;
   const octokit = github.getOctokit(token);
@@ -47,26 +59,28 @@ async function run(): Promise<void> {
   const { data: issue } = await octokit.rest.issues.get(octokitArgs);
 
   if (label && !hasLabel(issue.labels, label)) {
-    core.info(`Issue ${issue.title} does not have label ${label}. Skipping.`);
+    info(`Issue ${issue.title} does not have label ${label}. Skipping.`);
     return;
   }
 
-  const { data: comments } = await octokit.rest.issues.listComments(octokitArgs);
+  const { data: comments } = await octokit.rest.issues.listComments(
+    octokitArgs
+  );
 
   if (comments.length === 0) {
-    core.warning(`Issue ${issue.title} does not have any comments. Skipping.`);
+    warning(`Issue ${issue.title} does not have any comments. Skipping.`);
     return;
   }
 
   const body = issueBody(issue, comments);
   octokit.rest.issues.update({ ...octokitArgs, body });
-  core.notice(`Rolled up ${comments.length} comments to issue ${issue.title}`);
+  notice(`Rolled up ${comments.length} comments to issue ${issue.title}`);
 }
 
 try {
   run();
 } catch (error) {
-  if (error instanceof Error) core.setFailed(error.message);
+  if (error instanceof Error) setFailed(error.message);
 }
 
 export { hasLabel, issueBody, run };
